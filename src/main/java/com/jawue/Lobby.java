0beb673b47fe136d2baf4c720186a5d95f086cb5
jan_wue <jan_wue@proton.me>
2023-06-18 14:37:34 +0200

    release 1.0

--- src/main/java/com/jawue/Lobby.java
+++ src/main/java/com/jawue/Lobby.java
@@ -2,26 +2,31 @@ package com.jawue;
 
 import com.jawue.shared.message.ConnectMessage;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class Lobby extends Thread{
   BlockingQueue<PlayerConnection> boundedQueue = new LinkedBlockingQueue<>();
-
+  List<PlayerConnection> players = new ArrayList<>();
   @Override
   public void run() {
     super.run();
     System.out.println("Thread started lobby runs");
     while(true) {
      try {
-       PlayerConnection player1 = boundedQueue.take();
-       player1.sendMessage(new ConnectMessage());
 
-       PlayerConnection player2 = boundedQueue.take();
+       if(players.size() != 2) {
+         PlayerConnection player = boundedQueue.take();
+         player.sendMessage(new ConnectMessage());
+         players.add(player);
 
-       player2.sendMessage(new ConnectMessage());
-       Game game = new Game(player1, player2);
-       game.start();
+       } else {
+         Game game = new Game(players.get(0), players.get(1));
+         game.start();
+         players.clear();
+       }
      } catch (Exception exception) {
 
      }
@@ -31,5 +36,11 @@ public class Lobby extends Thread{
   public void addPlayerConnection(PlayerConnection playerConnection) {
     boundedQueue.add(playerConnection);
   }
+
+  public void removePlayerConnection(PlayerConnection playerConnection) {
+    boundedQueue.remove(playerConnection);
+    players.remove(playerConnection);
+  }
+
 }
 
